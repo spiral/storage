485b801453844ec88032b4ff30757129b6faece6
Evgeny Barinov <evgeny.barinov@spiralscout.com>
2021-01-13 09:59:48 +0400

    [feature]: AV-52 - add async tests

--- src/Enum/AdapterName.php
+++ src/Enum/AdapterName.php
@@ -8,7 +8,6 @@ abstract class AdapterName
 {
     public const LOCAL = 'local';
     public const AWS_S3 = 'awsS3';
-    public const ASYNC_AWS_S3 = 'asyncAwsS3';
     public const FTP = 'ftp';
     public const SFTP = 'sftp';
     public const MEMORY = 'memory';
@@ -16,7 +15,6 @@ abstract class AdapterName
     public const ALL = [
         self::LOCAL,
         self::AWS_S3,
-        self::ASYNC_AWS_S3,
         self::FTP,
         self::SFTP,
         self::MEMORY,
--- tests/StorageEngine/Unit/Config/DTO/ServerInfo/Aws/AwsS3InfoTest.php
+++ tests/StorageEngine/Unit/Config/DTO/ServerInfo/Aws/AwsS3InfoTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Spiral\StorageEngine\Tests\Unit\Config\DTO\ServerInfo\Aws;
 
 use Aws\S3\S3Client;
+use League\Flysystem\AsyncAwsS3\AsyncAwsS3Adapter;
 use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 use League\Flysystem\AwsS3V3\PortableVisibilityConverter;
-use League\Flysystem\Visibility;
 use Spiral\Core\Exception\ConfigException;
 use Spiral\StorageEngine\Config\DTO\ServerInfo\Aws\AwsS3Info;
 use Spiral\StorageEngine\Enum\AdapterName;
@@ -50,6 +50,32 @@ class AwsS3InfoTest extends AbstractUnitTest
     /**
      * @throws StorageException
      */
+    public function testValidateSimpleAsync(): void
+    {
+        $options = [
+            AwsS3Info::BUCKET_NAME => 'debugBucket',
+            AwsS3Info::CLIENT_NAME => $this->getAwsS3ClientDescription(),
+        ];
+
+        $serverInfo = new AwsS3Info(
+            'someServer',
+            [
+                AwsS3Info::CLASS_KEY => AsyncAwsS3Adapter::class,
+                AwsS3Info::DRIVER_KEY => AdapterName::AWS_S3,
+                AwsS3Info::OPTIONS_KEY => $options,
+            ]
+        );
+
+        $this->assertEquals(AsyncAwsS3Adapter::class, $serverInfo->getAdapterClass());
+
+        foreach ($options as $optionKey => $optionVal) {
+            $this->assertEquals($optionVal, $serverInfo->getOption($optionKey));
+        }
+    }
+
+    /**
+     * @throws StorageException
+     */
     public function testAdvancedUsage(): void
     {
         $options = [
@@ -63,6 +89,37 @@ class AwsS3InfoTest extends AbstractUnitTest
             'someServer',
             [
                 AwsS3Info::CLASS_KEY => AwsS3V3Adapter::class,
+                AwsS3Info::DRIVER_KEY => AdapterName::AWS_S3,
+                AwsS3Info::OPTIONS_KEY => $options,
+            ]
+        );
+
+        $this->assertTrue($advancedAwsS3Info->isAdvancedUsage());
+        foreach ($options as $optionKey => $optionVal) {
+            $this->assertEquals($optionVal, $advancedAwsS3Info->getOption($optionKey));
+        }
+
+        $visibilityConvertor = $advancedAwsS3Info->getVisibiltyConverter();
+        $this->assertInstanceOf(PortableVisibilityConverter::class, $visibilityConvertor);
+        $this->assertSame($visibilityConvertor, $advancedAwsS3Info->getVisibiltyConverter());
+    }
+
+    /**
+     * @throws StorageException
+     */
+    public function testAdvancedUsageAsync(): void
+    {
+        $options = [
+            AwsS3Info::BUCKET_NAME => 'debugBucket',
+            AwsS3Info::CLIENT_NAME => $this->getAwsS3ClientDescription(),
+            AwsS3Info::PATH_PREFIX => 'somePrefix',
+            AwsS3Info::VISIBILITY => $this->getAwsS3VisibilityOption(),
+        ];
+
+        $advancedAwsS3Info = new AwsS3Info(
+            'someServer',
+            [
+                AwsS3Info::CLASS_KEY => AsyncAwsS3Adapter::class,
                 AwsS3Info::DRIVER_KEY => AdapterName::AWS_S3,
                 AwsS3Info::OPTIONS_KEY => $options,
             ]
